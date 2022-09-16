4be4e4f2b3421dcff9ee970787163f1c37e2491c
femot <16308228+femot@users.noreply.github.com>
2022-09-16 16:28:33 +0200

    remove old VLC audio player

--- pkg/audioplayer/audioplayer.go
+++ pkg/audioplayer/audioplayer.go
@@ -3,44 +3,12 @@ package audioplayer
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"time"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
 )
-
-const vlcPath = `C:\Program Files (x86)\VideoLAN\VLC\vlc.exe`
-
-type AudioPlayer struct {
-	cmd *exec.Cmd
-}
-
-func StartPlayer(path string) (*AudioPlayer, error) {
-	p := AudioPlayer{}
-	if _, err := os.Stat(path); err != nil {
-		return nil, err
-	}
-	p.cmd = exec.Command(vlcPath, "-I", "dummy", "--dummy-quiet", "--volume", "1", path, "vlc://quit")
-	return &p, p.cmd.Start()
-}
-
-func (v AudioPlayer) Stop() error {
-	if v.cmd.Process != nil {
-		return v.cmd.Process.Kill()
-	}
-	return nil
-}
-
-func (v AudioPlayer) Done() <-chan struct{} {
-	c := make(chan struct{})
-	go func() {
-		v.cmd.Wait()
-		close(c)
-	}()
-	return c
-}
 
 type AudioPlayer2 struct {
 	streamer beep.StreamSeekCloser
